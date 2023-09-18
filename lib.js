f4aa85a12dd74fb025f564eab1e80e46d163fd69
roberto montalti <roberto.montalti00@gmail.com>
2023-09-18 15:47:38 +0200

    bench: simple benchmark with tinybench

--- lib.js
+++ lib.js
@@ -117,11 +117,13 @@ const resolveMatchingStructure = (patternStruct, againstStruct) => {
   if (typeof patternStruct === 'object' && typeof againstStruct === 'object') {
     const keys = Object.keys(patternStruct)
     const result = {}
-    for (let i = 0; i < keys.length; i++)
-      result[keys[i]] = resolveMatchingStructure(
-        patternStruct[keys[i]],
-        againstStruct[keys[i]]
+    for (let i = 0; i < keys.length; i++) {
+      const key = keys[i]
+      result[key] = resolveMatchingStructure(
+        patternStruct[key],
+        againstStruct[key]
       )
+    }
     return result
   }
 
